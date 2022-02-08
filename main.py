db1dc7c1c980f85656097870b0ec6855b3cc1f40
zoeellis <92262366+zoeellis@users.noreply.github.com>
2022-02-08 18:26:48 +0000

    Imput

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-echo "# UCDPA_zoeellis" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/zoeellis/UCDPA_zoeellis.git
-git push -u origin main
+print("youdidit")
