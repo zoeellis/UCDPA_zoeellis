2c5473c79fafeae3ff4697090873eb8715f8f9c1
zoeellis <92262366+zoeellis@users.noreply.github.com>
2022-02-09 15:40:58 +0000

    Import CVS file

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#import CSV file inot Pandas DataFrame
+#import CSV file into Pandas DataFrame
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+data = pd.read_csv("/Users/zoeellis/Library/Containers/com.microsoft.Excel/Data/Desktop/Homelessness Report December 2021.csv")
+data.head()
+print(data)
+
+
